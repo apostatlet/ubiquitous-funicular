d2aa2378609e3525bddc6e8c0d760256281ef729
apostatlet <85199396+apostatlet@users.noreply.github.com>
2024-11-03 10:20:38 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Cache name and files to cache
+const cacheName = 'unit-converter-cache';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json'
+];
+
+// Install event - caching the app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Fetch event - serving cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
